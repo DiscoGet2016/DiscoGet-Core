f00953fed71ca8c09a7beeaeac549d067026d7a8
Enrique <esjr81@gmail.com>
2016-08-20 17:55:13 -0700

    Included SearchActivity and modified MainActivity java and xml files to start SearchActivity. SearchActivity is blank for now.

--- app/src/main/java/com/discoget/test/discoget_core/MainActivity.java
+++ app/src/main/java/com/discoget/test/discoget_core/MainActivity.java
@@ -1,8 +1,10 @@
 package com.discoget.test.discoget_core;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the button that starts SearchActivity
+        Button button_search = (Button) findViewById(R.id.btn_searchActivity);
+
+        // Set a clickListener on that
+        button_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create a new intent to open the {@link SearchActivity}
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+
+                // Start the new activity
+                startActivity(searchIntent);
+            }
+        });
     }
 
     public void AddMessage(View view) {
@@ -32,4 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 }
