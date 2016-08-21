0d39d0b61f1756338ad4cd0b54d6cb134b79e314
Enrique <esjr81@gmail.com>
2016-08-20 18:45:44 -0700

    Cleaned all code. No new functionality from meeting on 8/20/16.

--- app/src/main/java/com/discoget/test/discoget_core/SearchActivity.java
+++ app/src/main/java/com/discoget/test/discoget_core/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.discoget.test.discoget_core;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+
+        getSupportActionBar().setTitle(R.string.toolbar_search_title);
+        getSupportActionBar().setIcon(R.drawable.ic_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
