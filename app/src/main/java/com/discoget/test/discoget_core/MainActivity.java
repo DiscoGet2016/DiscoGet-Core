a3bef84312a7b6600bd6d39bdfc4631ebb21192a
Enrique <esjr81@gmail.com>
2016-08-26 10:22:48 -0700

    Minor fixes.

--- app/src/main/java/com/discoget/test/discoget_core/MainActivity.java
+++ app/src/main/java/com/discoget/test/discoget_core/MainActivity.java
@@ -21,10 +21,8 @@ public class MainActivity extends AppCompatActivity {
         button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 // Create a new intent to open the {@link SearchActivity}
                 Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
-
                 // Start the new activity
                 startActivity(searchIntent);
             }
@@ -37,10 +35,8 @@ public class MainActivity extends AppCompatActivity {
         button_layoutSwitcher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // Create a new intent to open the {@link SearchActivity}
+                // Create a new intent to open the {@link LayoutSwitcher activity}
                 Intent layoutIntent = new Intent(MainActivity.this, LayoutSwitcher.class);
-
                 // Start the new activity
                 startActivity(layoutIntent);
             }
@@ -48,24 +44,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void AddMessage(View view) {
-
-        //Button btn_One = (Button);
-        TextView txt_msgOne;
-        TextView txt_mytext;
-
-
-        txt_msgOne = (TextView) findViewById(R.id.txt_msgOne);
-        txt_mytext = (TextView) findViewById(R.id.txt_mytext);
-
+        TextView txt_mytext = (TextView) findViewById(R.id.txt_mytext);
         String msgOne = "Hi Team!!!";
-        String msgTwo = "How is it going?";
-
-        txt_msgOne.setText(msgOne);
-        txt_mytext.setText(msgTwo);
-
-
-
+        txt_mytext.setText(msgOne);
     }
-
-
 }
