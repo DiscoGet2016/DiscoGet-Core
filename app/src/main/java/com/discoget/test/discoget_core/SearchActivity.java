f1231a7788f5c9ebb1426454b119b4fa58ebe6b0
Enrique <esjr81@gmail.com>
2016-08-25 18:17:09 -0700

    Created ScanActivity accessed from SearchActivity. Camera is opened in ScanActivity. Barcodes are detected and displayed as a string in a TextView.

--- app/src/main/java/com/discoget/test/discoget_core/SearchActivity.java
+++ app/src/main/java/com/discoget/test/discoget_core/SearchActivity.java
@@ -1,9 +1,12 @@
 package com.discoget.test.discoget_core;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -17,6 +20,22 @@ public class SearchActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle(R.string.toolbar_search_title);
         getSupportActionBar().setIcon(R.drawable.ic_menu);
+
+        // Find the button that starts SearchActivity
+        Button button_scan = (Button) findViewById(R.id.btn_scan);
+
+        // Set a clickListener on that
+        button_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create a new intent to open the {@link SearchActivity}
+                Intent scanIntent = new Intent(SearchActivity.this, ScanActivity.class);
+
+                // Start the new activity
+                startActivity(scanIntent);
+            }
+        });
     }
 
     @Override
